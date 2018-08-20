c66745ee06857b0fdca646621b4ca73e69727364
Guest User <Guest@epicodus-50.local>
2018-08-20 11:13:19 -0700

    multi page lessons

---
+++ src/app/album-detail/album-detail.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Album } from '../album.model';
+
+@Component({
+  selector: 'app-album-detail',
+  templateUrl: './album-detail.component.html',
+  styleUrls: ['./album-detail.component.css']
+})
+export class AlbumDetailComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute, private location: Location) { }
+
+  ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.albumId = parseInt(urlParameters['id']);
+    });
+  }
+  //no good
+  // newThis = this;
+  // ngOnInit() {
+  //   this.route.params.forEach(function(urlParameters){
+  //     this.newThis.albumId = parseInt(urlParameters['id'])
+  //   });
+  // }
+
+  albumId: number = null;
+}
