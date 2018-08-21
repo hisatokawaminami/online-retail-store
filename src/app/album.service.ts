f5fb5f1c58b63ef065478851d1d096bfb38f7d63
Guest User <Guest@epicodus-50.local>
2018-08-21 15:05:52 -0700

    add id and retreive data from database

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
-import { ALBUMS } from './mock-albums';
+// import { ALBUMS } from './mock-albums';
 import { AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 
 @Injectable()
@@ -18,11 +18,19 @@ export class AlbumService {
     // return ALBUMS;
   }
 
-  getAlbumById(albumId: number){
-    for (var i = 0; i <= ALBUMS.length -1; i++) {
-      if (ALBUMS[i].id === albumId) {
-        return ALBUMS[i];
-      }
-    }
+  addAlbum(newAlbum: Album){
+    this.albums.push(newAlbum);
   }
+
+  getAlbumById(albumId: string){
+   return this.database.object('albums/' + albumId);
+ }
+  // getAlbumById(albumId: number)
+  // {
+  //   for (var i = 0; i <= ALBUMS.length -1; i++) {
+  //     if (ALBUMS[i].id === albumId) {
+  //       return ALBUMS[i];
+  //     }
+  //   }
+  // }
 }
