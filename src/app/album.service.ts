56e269725b9d93f9d67c42eb5f09b33142e4d39d
Manuela Garcia <manuela.garcia.dev@gmail.com>
2018-08-22 09:16:30 -0700

    add delete function

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -40,4 +40,9 @@ export class AlbumService {
                                 description: localUpdatedAlbum.description
                               });
   }
+
+  deleteAlbum(localAlbumToDelete){
+    var albumEntryInFirebase = this.getAlbumById(localAlbumToDelete.$key);
+    albumEntryInFirebase.remove();
+  }
 }
